aecc4d519fa1fccadfa5937d4c1f8ae6a9a639e2
Documenter.jl <documenter@juliadocs.github.io>
2021-06-09 07:53:47 +0000

    build based on 86b9134

---
+++ 0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FooBar","category":"page"},{"location":"#FooBar","page":"Home","title":"FooBar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FooBar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FooBar]","category":"page"}]
+}
